0713d76405b812c2143af2a0925778b6fe2188ca
Nguyen Van Hung <nvh66725@gmail.com>
2023-08-09 01:41:51 +0700

    create user page

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <>I am Dashboard</>
+}
---
+++ src/app/login/page.js
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <main>
+      <h1>This is LoginPage</h1>
+    </main>
+  )
+}
